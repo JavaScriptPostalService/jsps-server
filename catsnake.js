39799b70de948465d508c3d5ac2904b16b782a11
wski <contact.mattdylan@gmail.com>
2016-04-08 12:56:38 -0400

    msgpack optimization

--- catsnake.js
+++ catsnake.js
@@ -30,9 +30,8 @@ channelLoader(ch => {
   // Note it's app.ws.use and not app.use
   app.ws.use(route.all('/', function* (next) {
     this.websocket.on('message', data => {
-      //let message = msgpack.decode(data);
-      let message = JSON.parse(data);
-      
+      let message = msgpack.decode(data);
+
       switch (message.metadata.type) {
 
         case 'subscribe':
--- modules/tools/sender.js
+++ modules/tools/sender.js
@@ -9,8 +9,7 @@ const msgpack = require("msgpack-lite");
 */
 const sender = function(data) {
   try {
-    return JSON.stringify(data);
-//    return msgpack.encode(data);
+    return msgpack.encode(data);
   } catch (e) {
     return JSON.stringify({
       error: 'server attempted to send invalid data to client.'
